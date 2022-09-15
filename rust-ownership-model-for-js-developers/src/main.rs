0c3f3185972b1af8db6c064ca172557885152f16
André Ferreira <contato@andreferreira.me>
2022-09-16 00:20:07 +0100

    Update example

--- rust-ownership-model-for-js-developers/src/main.rs
+++ rust-ownership-model-for-js-developers/src/main.rs
@@ -1,30 +1,49 @@
+#[derive(Debug)]
 struct Product;
 
 struct Config {
     debug_mode: bool,
 }
 
-struct ProductService {
-    config: Config,
-}
-struct BasketService {
-    config: Config,
+struct ProductService<'a> {
+    config: &'a Config,
 }
 
-impl ProductService {
-    fn new(config: Config) -> ProductService {
+struct BasketService<'a> {
+    config: &'a Config,
+}
+
+impl<'a> ProductService<'a> {
+    fn new(config: &Config) -> ProductService {
         ProductService { config: config }
+    }
+
+    fn get_product(&self, id: i32) -> Product {
+        if self.config.debug_mode {
+            println!("retrieving product for id: {:?}", id);
+        }
+
+        Product
     }
 }
 
-impl BasketService {
-    fn new(config: Config) -> BasketService {
+impl<'a> BasketService<'a> {
+    fn new(config: &Config) -> BasketService {
         BasketService { config: config }
+    }
+
+    fn add_product(&self, item: Product) {
+        if self.config.debug_mode {
+            println!("adding product {:?}", item);
+        }
     }
 }
 
 fn main() {
     let config = Config { debug_mode: true };
-    let _product_service = ProductService::new(config);
-    let _basket_service = BasketService::new(config);
+    let _product_service = ProductService::new(&config);
+    let _basket_service = BasketService::new(&config);
+
+    let product = _product_service.get_product(1);
+    _basket_service.add_product(product);
 }
